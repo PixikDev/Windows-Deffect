bbb379bd2ef1ced6bb1e15af58c6bb1c78366c3c
PixikDev <ilnazxuligan@gmail.com>
2025-11-15 15:20:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+
+print("Оиждайте обновления...")
+time.sleep(2)
+print("Скачивание...")
+time.sleep(3)
+print("."), time.sleep(1), print(".."), time.sleep(1), print("..."), time.sleep(1), print("...."), time.sleep(1), print(".....")
+time.sleep(5)
+print("Проверка завершена!")
+time.sleep(1)
+print("Поздравляю! Ты на ратке!")
